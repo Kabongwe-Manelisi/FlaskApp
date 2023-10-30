1949aa50a232cf58af1d1f3a2b1b6f097b7472b8
manelisi <kabongweinvestments@gmail.com>
2023-10-30 18:02:13 +0000

    mysql.connector

--- app.py
+++ app.py
@@ -1,41 +1,57 @@
+import mysql.connector as mysql
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from tabulate import tabulate 
+
+HOST = 'localhost'
+DATABASE = 'appTest1'
+USER = 'app1.0.0'
+PASSWORD = ''
+
+db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+print(db_connection.get_server_info())
+
+cursor = db_connection.cursor()
+cursor.execute('select database();')
+database_name = cursor.fetchone()
+print('[+] you are connected to the database:', database_name)
+
+
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    with app.app_context ():
-            db.create_all()
 
-    def __repr__(self):
+Todo = cursor.fetchone()
+
+def ___repr__(self):
         
         return '<Task %r>' % self.id
 
-
             
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST' , 'GET'])
 def index():
     if request.method =='POST':
-         task_content = request.form['content']
-         new_task =Todo(content=task_content)
+        task_content =request.form['content']
+        new_task = Todo (content=task_content)
 
-         try:
-              db.session.add(new_task)
-              db.session.commit()
-              return redirect('/')
-         except:
-              return 'there was an issuse adding your task'
+        try:
+            cursor.execute("""
+insert into Todo (id, content) values (
+                           %s,%s
+)
+""", params=(id))
+            db_connection.add(new_task)
+            db_connection.commit()
+            return redirect('/')
+        except:
+            return 'there was an issue adding your task'
+        
     else:
-         tasks = Todo.query.order_by(Todo.date_created).all()
-         return render_template('index.html', tasks=tasks)
-
+        sql_statement = 'select * from Todo order by id'
+        cursor.execute(sql_statement)
+        tasks=cursor.fetchall()
+        # tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
 if __name__ == '__main__':
     app.run(debug=True)
